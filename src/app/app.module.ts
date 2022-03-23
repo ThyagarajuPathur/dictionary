be55af82a75fc9f3acc59b439e53555bdadff340
Thyagaraju <thyagaraju.ptr@gmail.com>
2022-03-23 10:29:00 +0530

    checking in error code- angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { MatSlider } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSlideToggle,
+    MatSlider
   ],
   providers: [],
   bootstrap: [AppComponent]
